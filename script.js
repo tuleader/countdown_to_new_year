71905da12a9878998d9b3ca0e8b0475de116b3fc
tuleader <ngoctuta.it1@gmail.com>
2025-03-01 15:10:30 +0700

    add ads

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let dayBox = document.getElementById("day-box");
 let hrBox = document.getElementById("hr-box");
 let minBox = document.getElementById("min-box");
 let secBox = document.getElementById("sec-box");
-let endDate = new Date(2025, 0, 29);
+let endDate = new Date(2026, 1, 17);
 let endTime = endDate.getTime();
 
 function countdown() {
